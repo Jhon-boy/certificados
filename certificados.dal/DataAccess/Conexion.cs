9b85cababc477b8b1736516808e9254cf6cb9e7b
Javier1818 <geovannygsistemas@gmail.com>
2024-11-18 19:14:02 -0500

    Spring_001: Clase de Conexion

--- certificados.dal/DataAccess/Conexion.cs
+++ certificados.dal/DataAccess/Conexion.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace certificados.dal.DataAccess
 {
-    internal class Conexion
+    internal class Conexion(IConfiguration configuration)
     {
+        private readonly string? _cadenaConexion = configuration.GetConnectionString("DefaultConnection");
+
+        public SqlConnection Conectar() {
+            SqlConnection conected;
+            conected = new SqlConnection(_cadenaConexion);
+            return conected;
+        }
     }
 }
