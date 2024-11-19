f34957e13bbde74b6aaac55eecd0a0269c47af81
Javier1818 <geovannygsistemas@gmail.com>
2024-11-18 22:02:38 -0500

    Spring_001: Implementacion de la clase RolService

--- certificados.services/Services/RolService.cs
+++ certificados.services/Services/RolService.cs
@@ -1,12 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using certificados.dal.DataAccess;
+using certificados.models.Context;
+using certificados.models.Entitys.dbo;
 
 namespace certificados.services.Services
 {
-    internal class RolService
+    public class RolService
     {
+        private readonly AppDbContext appDbContext;
+
+        public RolService(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public bool crearRol(Trol rolEntidad)
+        {
+            TrolDA rolDA = new TrolDA(appDbContext);
+            try
+            {
+                rolDA.insertarRol(rolEntidad);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ex.GetBaseException();
+                return false;
+            }
+        }
+
+        public bool actualizarRol(Trol rolEntidad)
+        {
+            TrolDA rolDA = new TrolDA(appDbContext);
+            try
+            {
+                rolDA.modificarRol(rolEntidad);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ex?.GetBaseException();
+                return false;
+            }
+        }
+
+        public Trol buscarRol(int idRol)
+        {
+            TrolDA rolDA = new TrolDA(appDbContext);
+            try
+            {
+                return rolDA.buscarRol(idRol);
+            }
+            catch (Exception ex)
+            {
+                ex?.GetBaseException();
+                return null;
+            }
+        }
+
+        public List<Trol>? listarRol()
+        {
+            TrolDA rolDA = new TrolDA(appDbContext);
+            try
+            {
+                return rolDA.listarRol();
+            }
+            catch (Exception ex)
+            {
+                ex.GetBaseException();
+                return null;
+            }
+        }
+
+        public bool eliminarRol(Trol rolEntidad)
+        {
+            TrolDA rolDA = new TrolDA(appDbContext);
+            try
+            {
+                rolDA.eliminarRol(rolEntidad);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ex.GetBaseException();
+                return false;
+            }
+        }
     }
+
+
 }
