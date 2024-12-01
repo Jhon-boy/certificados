1efd39826e7c7833d9f10ba66ca7b74f69013f9b
Javier1818 <geovannygsistemas@gmail.com>
2024-12-01 11:38:00 -0500

    Spring_001: Interfaz para le registro de planificaciones

---
+++ certificados.web/wwwroot/js/planificacion.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('#planificacion-form').on('submit', function (e) {
+        e.preventDefault();
+        alert('Formulario guardado exitosamente.');
+        // Puedes agregar lógica para enviar los datos al servidor aquí.
+    });
+});
