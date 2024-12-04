25f494c1bab132ef8ecee9f5fa2423126e33450a
Javier1818 <geovannygsistemas@gmail.com>
2024-12-03 22:22:01 -0500

    Spring_001: Actualizacion de path css

---
+++ certificados.web/wwwroot/js/dashboard.js
@@ -0,0 +1,4 @@
+﻿// Función para controlar el despliegue del sidebar en pantallas pequeñas
+document.getElementById('sidebarCollapse').addEventListener('click', function () {
+    document.getElementById('sidebar').classList.toggle('show');
+});
