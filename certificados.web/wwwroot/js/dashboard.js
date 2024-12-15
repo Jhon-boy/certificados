5a0a50f3d85703beb8f671160fdd94785c24799e
Javier1818 <geovannygsistemas@gmail.com>
2024-12-15 14:43:34 -0500

    Spring_001: Interfaz para Grupo

--- certificados.web/wwwroot/js/dashboard.js
+++ certificados.web/wwwroot/js/dashboard.js
@@ -1,4 +1,1 @@
-﻿// Función para controlar el despliegue del sidebar en pantallas pequeñas
-document.getElementById('sidebarCollapse').addEventListener('click', function () {
-    document.getElementById('sidebar').classList.toggle('show');
-});
+﻿
