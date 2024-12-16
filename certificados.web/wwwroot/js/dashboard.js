4e21004e9cbe5b6ebe738d5462efc9b14e38dd66
Javier1818 <geovannygsistemas@gmail.com>
2024-12-15 19:17:06 -0500

    Spring_001: Ajustes de del menu sidebar

--- certificados.web/wwwroot/js/dashboard.js
+++ certificados.web/wwwroot/js/dashboard.js
@@ -1,1 +1,12 @@
-﻿
+﻿function toggleCollapse(submenuId) {
+    const submenu = document.getElementById(submenuId);
+    const arrow = submenu.previousElementSibling.querySelector('.arrow');
+
+    if (submenu.style.display === "block") {
+        submenu.style.display = "none";
+        arrow.classList.remove("rotate");
+    } else {
+        submenu.style.display = "block";
+        arrow.classList.add("rotate");
+    }
+}
