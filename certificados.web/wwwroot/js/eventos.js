87443039cae7a56bfc78cd38f9e1faacdd471997
Jhon Cuvi <jhoncuvi12@gmail.com>
2025-01-12 01:13:19 -0500

    Compleo

--- certificados.web/wwwroot/js/eventos.js
+++ certificados.web/wwwroot/js/eventos.js
@@ -72,9 +72,9 @@ async function cargarDatosTipoEvento() {
                     <td>${Utils.formatFecha(tipo.fCreacion)} </td>
                     <td>${Utils.formatFecha(tipo.fModificacion)} </td>
                     <td>${tipo.usuarioIngreso}</td>  
+                    <td>${tipo.usuarioActualizacion ?? 'No Actualizado'}</td>  
                     <td>
-                        <i class="bi bi-pencil-fill text-success me-3" style="cursor: pointer;" onclick="editarTipoEvento(${tipo.idtipoevento})" data-bs-toggle="tooltip" data-bs-placement="top" title="Editar evento"></i>
-                        <i class="bi bi-trash-fill text-danger me-3" style="cursor: pointer;" onclick="eliminarTipoEvento(${tipo.idtipoevento})" data-bs-toggle="tooltip" data-bs-placement="top" title="Eliminar evento"></i>
+                        <i class="bi bi-pencil-fill text-success me-3" style="cursor: pointer;" onclick="editarTipoEvento(${tipo.idtipoevento})" data-bs-toggle="tooltip" data-bs-placement="top" title="Editar evento"></i> 
                     </td>
                 </tr>
             `;
@@ -92,6 +92,48 @@ async function cargarDatosTipoEvento() {
     }
 }
 
+async function crearTipoEvento(event) {
+    event.preventDefault(); 
+
+    if (!Utils.validarFormulario('formAgregarTipo')) {
+        Utils.showToast("Corrige los errores en el formulario", "warning");
+        return;
+    }
+    const ModalEdit = document.getElementById('modalAgregarTipo');
+    const modalBootstrap = bootstrap.Modal.getInstance(ModalEdit);
+    try {
+        const payloadCreate = {
+            "Nombre": `${document.getElementById('nombreTipoC').value}`,
+            "Descripcion": `${document.getElementById('descripcionTipoC').value}`,
+            "usuarioIngreso": `${userInfoEv.idUsuario}`
+        }
+        const createEvent = await Utils.httpRequest(
+            `${Utils.path}/tipoEvento/crear`,
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(payloadCreate)
+            },
+            true);
+        if (createEvent.cod == Utils.COD_OK) {
+            Utils.showToast("Evento creado", "success");
+            cargarDatosTipoEvento();
+        } else {
+            const messageClient = rolesResponse.message || "Ocurrió un error inesperado.";
+            const messageTech = rolesResponse.data || null;
+            Utils.showErrorModal(messageClient, messageTech);
+        }
+
+    } catch (error) {
+        console.log(error);
+        Utils.showToast("Error al crear el tipo", "danger");
+    } finally {
+        if (modalBootstrap) {
+            modalBootstrap.hide();
+        }
+    }
+
+}
 async function editarevento(id) {
     try {
 
@@ -132,11 +174,56 @@ async function editarevento(id) {
 async function confirmarEditarEvento(event) {
     event.preventDefault();
 
-    if (!validarFormulario('formEditarEvento')) {
+    if (!Utils.validarFormulario('formEditarEvento')) {
         Utils.showToast("Corrige los errores en el formulario", "warning");
         return;
     } 
-    Utils.showToast("Formulario válido. Procesando datos...", "success");
+    const ModalEdit = document.getElementById('modalEditarEvento');
+    const modalBootstrap = bootstrap.Modal.getInstance(ModalEdit);
+    try {
+        const form = document.getElementById("formEditarEvento");
+        const payloadUpdate = {
+            "idevento": `${parseInt(form.querySelector("#idEventoE").value, 10)}`,
+            "FechaInicio": `${new Date(form.querySelector("#FechaInicio").value).toISOString()}`,
+            "FechaFin": `${new Date(form.querySelector("#FechaFin").value).toISOString()}`,
+            "Horas": `${parseInt(form.querySelector("#Horas").value, 10)}`,
+            "Lugar": `${form.querySelector("#lugarE").value}`,
+            "ConCertificado": `${form.querySelector("#conCertificado").value === "true" ? 1 : 0}`,
+            "Periodo": `${form.querySelector("#periodoEvento").value}`,
+            "Tematica": `${form.querySelector("#tematicaEvento").value}`,
+            "Dominio": `${form.querySelector("#dominioE").value}`,
+            "IdGrupo": `${parseInt(form.querySelector("#idGrupoE").value, 10)}`,
+            "IdModalidad": `${parseInt(form.querySelector("#modalidadE").value, 10)}`,
+            "IdTipoEvento": `${parseInt(form.querySelector("#ttipoEvento").value, 10)}`,
+            "IdDecanato": `${parseInt(form.querySelector("#idDecanatoE").value, 10)}`,
+            "UsuarioActualizacion": `${userInfoEv.idUsuario}`
+        };
+
+        const updateEvent = await Utils.httpRequest(
+            `${Utils.path}/evento/modificar`,
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(payloadUpdate)
+            },
+            true);
+
+        if (updateEvent.cod == Utils.COD_OK) {
+            cargarDatosEventos();
+            Utils.showToast("Evento Actualizado", "success");
+        } else {
+            const messageClient = rolesResponse.message || "Ocurrió un error inesperado.";
+            const messageTech = rolesResponse.data || null;
+            Utils.showErrorModal(messageClient, messageTech);
+        }
+    } catch (error) {
+        console.log(error);
+        Utils.showToast("Error al actualizar el evento", "danger");
+    } finally {
+        if (modalBootstrap) {
+            modalBootstrap.hide();
+        }
+    }
 
 }
 async function editarTipoEvento(idTipo) {
@@ -176,15 +263,79 @@ async function editarTipoEvento(idTipo) {
     }
 }
 
-async function confirmarEditarTipoEvento() {
-    if (!validarFormulario('formEditarTipoEvento')) {
+async function confirmarEditarTipoEvento(event) {
+    event.preventDefault();
+
+    const form = document.getElementById('formEditarTipoEvento');
+
+    if (!Utils.validarFormulario('formEditarTipoEvento')) {
         Utils.showToast("Corrige los errores en el formulario", "warning");
         return;
     } 
-    Utils.showToast("Formulario válido. Procesando datos...", "success");
+    const ModalEdit = document.getElementById('modalEditarEventoTipo');
+    const modalBootstrap = bootstrap.Modal.getInstance(ModalEdit);
+
+    const payloadUpdate = {
+        "Idtipoevento": `${parseInt(form.querySelector("#idTipoEventoE").value, 10)}`,
+        "Nombre": `${form.querySelector("#nombreTipoEvento").value}`,
+        "Descripcion": `${form.querySelector("#descripcionTipoEvento").value}`,
+        "UsuarioActualizacion": `${userInfoEv.idUsuario}`,
+    };
+    try {
+        const updateEvent = await Utils.httpRequest(
+            `${Utils.path}/tipoEvento/modificar`,
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(payloadUpdate)
+            },
+            true);
+        if (updateEvent.cod == Utils.COD_OK) {
+            cargarDatosTipoEvento();
+            Utils.showToast("Evento Actualizado", "success");
+        } else {
+            const messageClient = rolesResponse.message || "Ocurrió un error inesperado.";
+            const messageTech = rolesResponse.data || null;
+            Utils.showErrorModal(messageClient, messageTech);
+        }
+    } catch (error) {
+        console.log(error);
+        Utils.showToast("Error al actualizar el evento", "danger");
+    } finally {
+        if (modalBootstrap) {
+            modalBootstrap.hide();
+        }
+    }
 
 }
+async function buscarEvento() { 
+    const idEventoB = document.getElementById('buscarevento').value;
+    console.log(idEventoB)
 
+    const getEvento = await getEventoById(idEventoB);
+    if (getEvento.cod == Utils.COD_OK && getEvento.data != null) {
+        const evento = getEvento.data;
+
+        // Asignar valores a los campos
+        document.getElementById("lugarE").value = evento.lugar || '';
+        document.getElementById("tematicaE").value = evento.tematica || '';
+        document.getElementById("dominioE").value = evento.dominio || '';
+        document.getElementById("modalidadE").value = evento.tmodalidad.nombre || '';
+        document.getElementById("tipoEventoE").value = evento.ttipoEvento.nombre || '';
+        document.getElementById("decanatoE").value = evento.tdecanato.nombre || '';
+        document.getElementById("horasEvento").value = evento.horas || '';
+        document.getElementById("fechaInicioE").value = evento.fechaInicio ? new Date(evento.fechaInicio).toLocaleString() : '';
+        document.getElementById("fechaFinE").value = evento.fechaFin ? new Date(evento.fechaFin).toLocaleString() : '';
+        document.getElementById("conCertificado").value = evento.conCertificado ? "Sí" : "No";
+        document.getElementById("uIngresoE").value = evento.usuarioIngreso || 'Admin';
+        document.getElementById("uActualizacionE").value = evento.usuarioActualizacion || 'No Actualizado';
+        document.getElementById("fIngresoEvento").value = Utils.formatFecha(evento.fCreacion);
+        document.getElementById("fActualizacionEvento").value = Utils.formatFecha(evento.fModificacion);
+
+    } else {
+        Utils.showToast("No existe el evento", 'info');
+    }
+}
 async function getEventoById(idEvento) {
     try {
         const payloadEdit = {
@@ -209,15 +360,44 @@ async function getEventoById(idEvento) {
     } catch (error) {
 
         console.log(error);
-        Utils.showToast("Error cargando datos iniciales", 'error');
+        Utils.showToast("Error cargando datos de busqueda", 'danger');
     }
 
 }
+
+
+async function getTipoEventoById(idEvento) {
+    try {
+        const payloadEdit = {
+            idtipoevento: idEvento
+        }
+        const EditResponse = await Utils.httpRequest(
+            `${Utils.path}/tipoEvento/id`,
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(payloadEdit)
+            },
+            true);
+        if (EditResponse.cod == Utils.COD_OK) {
+            return EditResponse;
+        } else {
+            const messageClient = rolesResponse.message || "Ocurrió un error inesperado.";
+            const messageTech = rolesResponse.data || null;
+            Utils.showErrorModal(messageClient, messageTech);
+        }
+
+    } catch (error) {
+
+        console.log(error);
+        Utils.showToast("Error cargando datos de busqueda", 'danger');
+    }
+
+}
+
 function eliminarEvento(id) {
     document.getElementById('modalEliminarEvento').dataset.idEvento = id;
     new bootstrap.Modal(document.getElementById('modalEliminarEvento')).show();
-}
-function editarTipoEvento(id) {
 }
 function eliminarTipoEvento(id) {
     document.getElementById('modalEliminarTipoEvento').dataset.idTipoEvento = id;
